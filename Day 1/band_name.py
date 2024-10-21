eadad1f452bfbd352a61829949387139a48d4ad5
gibbyynator <gibbyynatortech@gmail.com>
2024-10-20 20:02:56 -0400

    Add files via upload

---
+++ Day 1/band_name.py
@@ -0,0 +1,8 @@
+
+print("Lets create a band name!")
+town_input = input("Please enter your hometown.\n")
+pet_name_input = input("Please enter a childhood pets name.\n")
+
+band_name = town_input + " " + pet_name_input
+
+print("Your band name is: " + band_name)
